ffe5cde190a913692ed6cb0d400b47ef4faf54b7
putri01anisa-stars <putri01anisa@gmail.com>
2026-05-12 09:08:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,7 @@ import streamlit as st
 st.title("🎈KALKULATOR STOIKIOMETRI")
 st.write(
    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
-st.text("This is text\n[and more text](that's not a Markdown link).")
+)
+import streamlit as st
+
+st.balloons()
